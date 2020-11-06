7f6402c7cc87ed9c6178a55bde6a1ada714a4c08
TshepoDavid Molefe <tshepomolefeda@gmail.com>
2020-11-06 18:26:42 +0200

    added test methods

--- spec/enumerables_spec.rb
+++ spec/enumerables_spec.rb
@@ -14,4 +14,44 @@ describe Enumerable do
       expect(res1).to eq(res2)
     end
   end
+
+  describe '#my_each_with_index' do
+    it 'returns same as Ruby #each_with_index method' do
+      res1 = []
+      res2 = []
+      test_arr.my_each_with_index { |n, i| res1 << n + i }
+      test_arr.each_with_index { |n, i| res2 << n + i }
+      expect(res1).to eq(res2)
+    end
+  end
+
+  describe '#my_select' do
+    it 'returns same as Ruby #select method' do
+      expect(test_arr.my_select(&:even?)).to eq(test_arr.select(&:even?))
+    end
+  end
+
+  describe '#my_all' do
+    it 'returns same as Ruby #all? method' do
+      expect(test_arr.my_all?(Numeric)).to eq(test_arr.all?(Numeric))
+    end
+  end
+
+  describe '#my_any?' do 
+    it 'returns same as Ruby #any? method' do
+      expect(test_arr.my_any?(0)).to eq(test_arr.any?(0))
+    end
+  end
+
+  describe '#my_none?' do 
+    it 'returns same as Ruby #none? method' do
+      expect(test_arr.my_none?(0)).to eq(test_arr.none?(0))
+    end
+  end
+
+  describe '#my_count' do 
+    it 'returns same as Ruby #count method' do
+      expect(test_arr.my_count(7)).to eq(test_arr.count(7))
+    end
+  end
 end
